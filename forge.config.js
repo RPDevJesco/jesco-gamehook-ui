57016b959a1471164d155d11910ca61f15c736dd
GameDevMadeEasy <jglover88@hotmail.com>
2023-12-23 03:13:59 -0600

    Updates.

--- forge.config.js
+++ forge.config.js
@@ -1,24 +1,38 @@
 module.exports = {
   packagerConfig: {
     asar: true,
+    // additional packager configuration options
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        // configuration options for Windows Squirrel
+      },
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin'], // This will create a zip file for macOS
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        // configuration options for macOS DMG
+      },
+      platforms: ['darwin'], // This will create a DMG file for macOS
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        // configuration options for Debian packages
+      },
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        // configuration options for RPM packages
+      },
     },
   ],
   plugins: [
